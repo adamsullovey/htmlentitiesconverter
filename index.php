2dc53c65bcaf82d41c9213f52be7e1f13a10f9fe
Adam <chilly@oddlystudios.com>
2012-03-28 21:09:36 -0400

    minor tweaks

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ if (isset($_POST['content'])) {
 
 	$content = $_POST['content'];
 	$result = htmlentities($content, ENT_NOQUOTES, null, true);
+	
+	// run it again to convert the &s to &amp;s so the page doesn't re-encode html entities when displaying them
 	$result = htmlentities($result, ENT_NOQUOTES, null, true);
 	$result = str_replace('&amp;gt;', '>', $result);
 	$result = str_replace('&amp;lt;', '<', $result);
@@ -17,29 +19,25 @@ if (isset($_POST['content'])) {
 ?>
 
 <html>
-<head>
-<title>HTML Entities Converter</title>
-</head>
+	<head>
+		<title>HTML Entities Converter</title>
+	</head>
 
-<body>
+	<body>
+		<form method="POST">
+			<textarea name="content" style="width:100%; height:300px"><?= $content ?></textarea>
+			<input type="submit" />
+			<textarea name="result" style="width:100%; height:300px"><?= $result ?></textarea>
+			<input type="button" value="Copy to clipboard" onclick="copyToClipboard('<?= $result ?>')" />
+		</form>
+		
+		<pre><?= $result ?></pre>
 
+		<script type="text/javascript">
+			function copyToClipboard (text) {
+				window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);
+			}
+		</script>
 
-<form method="POST">
-<textarea name="content" style="width:100%; height:300px"><?= $content ?></textarea>
-<textarea name="result" style="width:100%; height:300px"><?= $result ?></textarea>
-<input type="submit" />
-</form>
-<pre><?= $result ?></pre>
-
-<script type="text/javascript">
-function copyToClipboard (text) {
-  window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);
-}
-
-
-//copyToClipboard('<?= $result ?>');
-
-</script>
-
-</body>
+	</body>
 </html>
